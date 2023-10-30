119b050f9321b089a5e278adb4d3bafe10f6fd78
jude <jude.sh@daum.net>
2023-10-30 16:02:37 +0900

    dev-server-config 추가

---
+++ dev-server-config.js
@@ -0,0 +1,4 @@
+exports.USE_SOURCE_MAP = true // sass 소스맵 사용 여부 - 기본값 false
+exports.DEV_SERVER_PORT = 8080 // 개발 서버 포트 번호 - 기본값 8080
+exports.DEV_FOLDER = 'dev' // 개발 서버 작동시 바라보는(기준이 되는) 폴더 이름 - 기본값 'dev'
+exports.PROD_FOLDER = 'build' // 빌드시 생성되는 폴더 이름 - 기본값 'build'
